6c8ef2dc9eeb54b019dfbddcfe0cac642e0eefcf
John Huang <johnqh@yahoo.com>
2024-10-21 11:32:10 -0700

    cannot build js

--- src/injections/renderers/viewcontrollers/UIWindow.tsx
+++ src/injections/renderers/viewcontrollers/UIWindow.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Typography } from '@mui/material';
-import { Renderable } from '../../../types/protocols';
 import ViewController from './ViewController';
 
-const UIWindow: React.FC<{ renderable?: Renderable | null }> = ({
-    renderable,
+const UIWindow: React.FC<{  }> = ({
 }) => {
+    const [renderable, _] = useState(null);
+
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
             <ViewController renderable={renderable} />
--- src/state/injections/Screen.ts
+++ src/state/injections/Screen.ts
@@ -1,53 +1,73 @@
 import { Nullable, com } from 'Sudojo';
-import { ScreenProtocol } from '../../types/protocols';
+import { Renderable, ScreenProtocol } from '../../types/protocols';
+import { useState } from 'react';
 
 class Screen implements ScreenProtocol {
     focused: Nullable<
         (p0: com.sudobility.sudokuschool.viewmodels.Renderable) => void
     >;
+
     top(): Nullable<com.sudobility.sudokuschool.viewmodels.Renderable> {
-        throw new Error('Method not implemented.');
+        const [renderable, _] = useState<Nullable<Renderable>>(null);
+        return renderable
     }
+
     current(): Nullable<com.sudobility.sudokuschool.viewmodels.Renderable> {
-        throw new Error('Method not implemented.');
+        const [current, _] = useState<Nullable<string>>(null);
+        if (!current) {
+            return null
+        }
+        const [renderable, __] = useState<Nullable<Renderable>>(null);
+        return renderable?.findById(current)
     }
+
     root(
         renderable: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         animated?: boolean,
         completion?: Nullable<(p0: boolean) => void>
     ): void {
-        throw new Error('Method not implemented.');
+        const [_, setRenderable] = useState<Nullable<Renderable>>(null);
+        setRenderable(renderable);
     }
+
     push(
         renderable: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         parent: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         animated?: boolean,
         completion?: Nullable<(p0: boolean) => void>
     ): void {
-        throw new Error('Method not implemented.');
+        const [_, setCurrent] = useState<Nullable<string>>(null);
+        setCurrent(renderable?.id);
     }
+
     pop(
         renderable: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         parent: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         animated?: boolean
     ): void {
-        throw new Error('Method not implemented.');
+        const [_, setCurrent] = useState<Nullable<string>>(null);
+        setCurrent(renderable?.parent?.id);
     }
+
     prompt(
         renderable: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         parent: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         animated?: boolean,
         completion?: Nullable<(p0: boolean) => void>
     ): void {
-        throw new Error('Method not implemented.');
+        const [_, setCurrent] = useState<Nullable<string>>(null);
+        setCurrent(renderable?.id);
     }
+
     dismiss(
         renderable: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         parent: Nullable<com.sudobility.sudokuschool.viewmodels.Renderable>,
         animated?: boolean
     ): void {
-        throw new Error('Method not implemented.');
+        const [_, setCurrent] = useState<Nullable<string>>(null);
+        setCurrent(renderable?.parent?.id);
     }
+
     message(
         message: com.sudobility.sudokuschool.protocols.ScreenMessage,
         blocking?: boolean,
